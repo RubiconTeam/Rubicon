f2f7e79fe2c5db48403d0e1c51a932bcdc03c2ed
Binpuki <30415792+Binpuki@users.noreply.github.com>
2024-11-11 00:39:09 -0600

    Untested UpdateHitObjects method in chart editor

--- addons/RubiconCharter/Scripts/Rulesets/Mania/ManiaEditorSpace.cs
+++ addons/RubiconCharter/Scripts/Rulesets/Mania/ManiaEditorSpace.cs
@@ -2,8 +2,5 @@ namespace Rubicon.Editor.Rulesets.Mania;
 
 public partial class ManiaEditorSpace : EditorSpace
 {
-    public override void UpdateHitObjects()
-    {
-        
-    }
+    protected override EditorHitObject CreateHitObject() => new ManiaEditorHitObject();
 }
