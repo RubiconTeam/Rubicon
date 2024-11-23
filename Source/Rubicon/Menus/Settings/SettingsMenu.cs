34a09e50a659ad7dadb04dd4a488d776beb767da
duo <98416575+DuoNineXcore@users.noreply.github.com>
2024-11-23 17:50:37 -0400

    Removed Section Button Template

--- Source/Rubicon/Menus/Settings/SettingsMenu.cs
+++ Source/Rubicon/Menus/Settings/SettingsMenu.cs
@@ -3,48 +3,47 @@ using Rubicon.Menus;
 using Rubicon.Data.Settings;
 using Rubicon.Data.Settings.Attributes;
 
+namespace Rubicon.Data.Settings;
 public partial class SettingsMenu : BaseMenu
 {
-	[NodePath("Main/Sidebar/AnimationPlayer")] private AnimationPlayer _sidebarAnimationPlayer;
-	[NodePath("Main/Sidebar")] private Control _sidebar;
-	[NodePath("Main/Sidebar/BG/Container")] private Control _buttonContainer;
-	[NodePath("Main/Sidebar/BG/Labels")] private Control _labelsParent;
+    [NodePath("Main/Sidebar/AnimationPlayer")] private AnimationPlayer _sidebarAnimationPlayer;
+    [NodePath("Main/Sidebar")] private Control _sidebar;
+    [NodePath("Main/UI/SectionButtons/PanelContainer/VBoxContainer")] private Control _sectionButtonContainer;
+    [NodePath("Main/UI/SectionButtons/PanelContainer/VBoxContainer/Section")] private Control _sectionButtonTemplate;
 
-	private const string ButtonTemplatePath = "res://Source/Rubicon.Menus/Settings/Resources/SectionButtonTemplate.tscn";
-	private const string DefaultIconPath = "res://Assets/UI/Menus/Settings/Gameplay.png";
+    public override void _Ready()
+    {
+        this.OnReady();
+        base._Ready();
 
-	public override void _Ready()
-	{
-		this.OnReady();
-		base._Ready();
-		
-		_sidebar.MouseEntered += () => _sidebarAnimationPlayer.Play("Enter");
-		_sidebar.MouseExited += () => _sidebarAnimationPlayer.Play("Leave");
+        _sidebar.MouseEntered += () => _sidebarAnimationPlayer.Play("Enter");
+        _sidebar.MouseExited += () => _sidebarAnimationPlayer.Play("Leave");
 
-		foreach (var prop in typeof(UserSettingsData).GetProperties())
-		{
-			var attribute = prop.PropertyType.GetCustomAttribute<RubiconSettingsSectionAttribute>();
-			if (attribute != null && attribute.GenerateInMenu) CreateSectionButton(attribute.Name, attribute.Icon);
-		}
-	}
-	
-	private void CreateSectionButton(string sectionName, Texture2D icon)
-	{
-		var buttonInstance = GD.Load<PackedScene>(ButtonTemplatePath).Instantiate<Control>();
-		buttonInstance.Name = sectionName;
+        // Hide the placeholder button
+        _sectionButtonTemplate.Visible = false;
 
-		var textureRect = buttonInstance.GetNode<TextureRect>("Icon");
-		var label = buttonInstance.GetNode<Label>("Text");
+        foreach (var prop in typeof(UserSettingsData).GetProperties())
+        {
+            var attribute = prop.PropertyType.GetCustomAttribute<RubiconSettingsSectionAttribute>();
+            if (attribute != null && attribute.GenerateInMenu) CreateSectionButton(attribute.Name, attribute.Icon);
+        }
+    }
 
-		textureRect.Texture = icon;
-		label.Text = sectionName;
-		label.Name = sectionName;
+    private void CreateSectionButton(string sectionName, Texture2D icon)
+    {
+        var buttonInstance = _sectionButtonTemplate.Duplicate() as Control;
+        if (buttonInstance != null)
+        {
+            buttonInstance.Name = sectionName;
 
-		_buttonContainer.AddChild(buttonInstance);
-		label.GetParent().RemoveChild(label);
-		_labelsParent.AddChild(label);
-		CallDeferred(nameof(PositionLabel), label, buttonInstance);
-	}
+            var textureRect = buttonInstance.GetNode<TextureRect>("Icon");
+            var label = buttonInstance.GetNode<Label>("Text");
 
-	private void PositionLabel(Label label, Control button) => label.Position = new Vector2(30, ((_buttonContainer.GetGlobalTransformWithCanvas().AffineInverse() * button.GetGlobalTransformWithCanvas().Origin).Y + button.Size.Y / 2) - label.Size.Y / 2);
+            textureRect.Texture = icon;
+            label.Text = sectionName;
+            label.Name = sectionName;
+
+            _sectionButtonContainer.AddChild(buttonInstance);
+        }
+    }
 }
