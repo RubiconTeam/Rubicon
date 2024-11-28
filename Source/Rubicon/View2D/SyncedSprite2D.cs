83fa842c5db73965ffee1d2c936ac7409c9763c3
Binpuki <30415792+Binpuki@users.noreply.github.com>
2024-11-28 06:59:40 -0600

    Playing -> Sync

--- Source/Rubicon/View2D/SyncedSprite2D.cs
+++ Source/Rubicon/View2D/SyncedSprite2D.cs
@@ -6,17 +6,13 @@ namespace Rubicon.View2D;
 #endif
 public partial class SyncedSprite2D : AnimatedSprite2D
 {
-	[Export] public bool Playing { get => GetPlaying(); set => SetPlaying(value); }
-
-	[Export] public new int Frame { get => base.Frame; set => SetFrame(value); }
-
-	[Export] public new float FrameProgress { get => base.FrameProgress; set => SetFrameProgress(value); }
+	[Export] public bool Sync { get => GetSync(); set => SetSync(value); }
 
 	[ExportGroup("Sync With"), Export] public AnimationPlayer AnimationPlayer;
 	
 	private double _time = 0.0;
 	private double _lastPlayerPosition = 0.0;
-	private bool _playing = false;
+	private bool _syncing = false;
 	
 	public override void _Process(double delta)
 	{
@@ -32,7 +28,7 @@ public partial class SyncedSprite2D : AnimatedSprite2D
 		if (Mathf.IsEqualApprox(currentPos, _lastPlayerPosition))
 			return;
 		
-		SyncWithAnimationPlayer(_playing ? currentPos - _lastPlayerPosition : 0.0);
+		SyncWithAnimationPlayer(_syncing ? currentPos - _lastPlayerPosition : 0.0);
 		_lastPlayerPosition = currentPos;
 	}
 
@@ -44,22 +40,19 @@ public partial class SyncedSprite2D : AnimatedSprite2D
 			
 		double fps = SpriteFrames.GetAnimationSpeed(Animation);
 
-		base.Frame = (int)Math.Floor(_time * fps);
-		base.FrameProgress = (float)(_time % (1 / fps) * fps);
+		Frame = (int)Math.Floor(_time * fps);
+		FrameProgress = (float)(_time % (1 / fps) * fps);
 	}
 
-	private bool GetPlaying()
+	private bool GetSync()
 	{
-		if (AnimationPlayer == null)
-			return IsPlaying();
-
-		return _playing;
+		return _syncing;
 	}
 	
-	private void SetPlaying(bool value)
+	private void SetSync(bool value)
 	{
-		_playing = value;
-		if (_playing)
+		_syncing = value;
+		if (_syncing)
 		{
 			if (AnimationPlayer != null)
 				return;
@@ -69,29 +62,5 @@ public partial class SyncedSprite2D : AnimatedSprite2D
 		}
 
 		Pause();
-	}
-
-	private new void SetFrame(int value)
-	{
-		FrameProgress = 0;
-
-		double frameRate = SpriteFrames.GetAnimationSpeed(Animation);
-		double fps = 1 / frameRate;
-		_time = value * fps + FrameProgress * fps;
-		if (_time < 0.0)
-			_time = 0.0;
-		
-		base.Frame = (int)Math.Floor(_time * frameRate);
-	}
-
-	private new void SetFrameProgress(float value)
-	{
-		base.SetFrameProgress(value);
-			
-		double frameRate = SpriteFrames.GetAnimationSpeed(Animation);
-		double fps = 1 / frameRate;
-		_time = Frame * fps + value * fps;
-		if (_time < 0.0)
-			_time = 0.0;
 	}
 }
